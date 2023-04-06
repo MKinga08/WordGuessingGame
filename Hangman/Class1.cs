d1f65c9c49e8e1a99c230615f0725b1c13d68e15
MKinga08 <dr.m.kinga08@gmail.com>
2023-04-06 12:01:26 +0200

    creating main method and classes

---
+++ Hangman/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hangman
+{
+    public class Game
+    {
+        public Game() 
+        { 
+
+        }
+    }
+}
--- Hangman/Program.cs
+++ Hangman/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Hangman
+{
+    class Program
+    {
+        public void Main(string[] args)
+        {
+
+        }
+    }
+}
